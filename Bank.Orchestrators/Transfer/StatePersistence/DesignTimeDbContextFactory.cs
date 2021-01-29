cb378751336d0f1cf05306425e17ba8d40df0962
zinchenkoaa <anton.zinchenko@gmail.com>
2021-01-29 22:08:23 +0300

    Завязал сагу на бд

---
+++ Bank.Orchestrators/Transfer/StatePersistence/DesignTimeDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using SeedWorks;
+using System.IO;
+
+namespace Bank.Orchestrators.Transfer.StatePersistence
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<TransferDbContext>
+    {
+        public TransferDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../Bank.Api"))
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            return new DbContextOptionsBuilder<TransferDbContext>()
+                .Do(builder => builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection")))
+                .PipeTo(builder => new TransferDbContext(builder.Options));
+        }
+    }
+}
---
+++ Bank.Orchestrators/Transfer/StatePersistence/Migrations/20210129182113_Initial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bank.Orchestrators.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TransferStates",
+                columns: table => new
+                {
+                    TransferId = table.Column<Guid>(nullable: false),
+                    CurrentState = table.Column<string>(nullable: false),
+                    SourceAccountId = table.Column<Guid>(nullable: false),
+                    TargetAccountId = table.Column<Guid>(nullable: false),
+                    Sum = table.Column<decimal>(nullable: false),
+                    Comment = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TransferStates", x => x.TransferId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TransferStates");
+        }
+    }
+}
